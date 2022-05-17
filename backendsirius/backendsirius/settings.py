c64d52196492c7a9cda66f78b27a1502239b95d6
dhifafaz <dhifaf.az@gmail.com>
2022-05-17 15:53:11 +0700

    Remove a hardcoded Django secret key

--- backendsirius/backendsirius/settings.py
+++ backendsirius/backendsirius/settings.py
@@ -68,7 +68,6 @@ INSTALLED_APPS = [
     'knox',
     'sirius_api.apps.SiriusApiConfig',
     'debug_toolbar',
-    'whitenoise.runserver_nostatic',
     
 ]
 
@@ -183,6 +182,10 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
+import dj_database_url
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
