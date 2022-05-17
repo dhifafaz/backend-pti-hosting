d4d36f12fa179ac621c5e1715c3e8d00d613ce99
dhifafaz <dhifaf.az@gmail.com>
2022-05-17 15:03:30 +0700

    Remove a hardcoded Django secret key

--- backendsirius/backendsirius/settings.py
+++ backendsirius/backendsirius/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     'knox',
     'sirius_api.apps.SiriusApiConfig',
     'debug_toolbar',
+    'whitenoise.runserver_nostatic',
     
 ]
 
@@ -77,6 +78,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -86,7 +88,6 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'backendsirius.urls'
@@ -173,6 +174,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
