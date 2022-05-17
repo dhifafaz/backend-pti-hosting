0c7114ee1577d2de996cffa6452684fa94d94e27
dhifafaz <dhifaf.az@gmail.com>
2022-05-17 13:50:24 +0700

    init

--- backendsirius/backendsirius/settings.py
+++ backendsirius/backendsirius/settings.py
@@ -175,10 +175,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-import django_heroku
-django_heroku.settings(locals())
+#import django_heroku
+#django_heroku.settings(locals())
